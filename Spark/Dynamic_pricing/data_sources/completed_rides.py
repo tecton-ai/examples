84c7509a977d59812f46d0a910549a10b7d3bf38
Felix Wang <wangfelix98@gmail.com>
2023-06-02 11:40:30 -0700

    Add completed rides data source

---
+++ Spark/Dynamic_pricing/data_sources/completed_rides.py
@@ -0,0 +1,14 @@
+from tecton import HiveConfig, BatchSource
+
+completed_rides_batch_config = HiveConfig(
+    database='rides',
+    table='completed_rides',
+    timestamp_field='TIMESTAMP'
+)
+
+completed_rides_batch = BatchSource(
+    name='completed_rides_batch',
+    batch_config=completed_rides_batch_config,
+    owner='felix@tecton.ai',
+    tags={'release': 'production'}
+)
